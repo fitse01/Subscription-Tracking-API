cb63bf69aff9c252ac3b1c52374edff49f2487f1
fitse01 <fitsumtafese01@gmail.com>
2025-03-20 21:57:13 +0300

    database setup

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import  Express from 'express';
+
 const app = Express();
+
 import {PORT} from "./config/env.js";
+
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to Subscription tracking API ")
--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import {config} from "dotenv";
 
 // config({path : '/.env'}); // if there only one env file
 config({path: `.env.${process.env.NODE_ENV || 'development'}.local`}); // if there exist more than one env file
-export const {PORT , NODE_ENV } = process.env;
+export const {PORT , NODE_ENV,DB_URI } = process.env;
